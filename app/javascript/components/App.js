e6379a3274f1499dcb0ecd1f012ba04b2234acf1
shekhar-patil <patilshekhar900@gmail.com>
2019-03-16 22:12:20 +0530

    add React

---
+++ app/javascript/components/App.js
@@ -0,0 +1,10 @@
+import React, { PureComponent } from 'react';
+class App extends React.Component {
+  render() { 
+    return ( <div>
+      hello brother
+    </div> );
+  }
+}
+ 
+export default App;
